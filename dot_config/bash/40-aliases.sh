f05c460bc9227075ba883eeeff64ae86350ac564
Dimitri Pietersz <pietersz@gmail.com>
2025-10-16 21:26:03 +0200

    add bash, git, ghostty, mise, psql and zsh configs

---
+++ dot_config/bash/40-aliases.sh
@@ -0,0 +1,82 @@
+# 50-aliases.sh — Custom Aliases
+
+# ------------------ Tools ------------------
+
+if command -v lazydocker >/dev/null; then
+  alias ld="lazydocker"
+fi
+
+if command -v lazygit >/dev/null; then
+  alias lg="lazygit"
+fi
+
+if command -v kubectl >/dev/null; then
+  alias k="kubectl"
+fi
+
+# if command -v zoxide >/dev/null; then
+#   alias cd="z"
+# fi
+
+# use bat instead of cat
+if command -v bat >/dev/null; then
+  alias cat="bat"
+fi
+
+## bat on debian based systems is batcat. Fix that.
+if command -v batcat >/dev/null; then
+  alias cat="batcat"
+fi
+
+# ------------------ Git ------------------
+
+alias gp="git pull"
+alias gs="git status"
+
+# ------------------ Directory Navigation ------------------
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~"
+alias cl="clear"
+alias dev="cd '$DEV'"
+alias scripts="cd '$SCRIPTS'"
+alias projects="cd '$PROJECTS'"
+alias dot="cd '$DOTFILES'"
+alias repos="cd '$REPOS'"
+alias glrepos="cd '$GLREPOS'"
+alias ghrepos="cd '$GHREPOS'"
+alias rwdot="cd '$REPOS/github.com/rwxrob/dot'"
+alias mvdot="cd '$REPOS/github.com/mischavandenburg/dotfiles'"
+alias tpdot="cd '$REPOS/github.com/ThePrimeagen/.dotfiles/'"
+alias ttdot="cd '$REPOS/github.com/techno-tim'"
+
+# ------------------ Shell / Config ------------------
+
+alias espansoconf="v '$DOTFILES/dot_config/espanso/match/base.yml'"
+alias bashconf="v ~/.bashrc"
+alias nvimconf="cd '$DOTFILES/dot_config/nvim && nvim init.lua'"
+alias sbr="source '$HOME/.bashrc'"
+
+# ------------------ Better ls commands ------------------
+alias ls="ls --color=auto"
+alias la="eza -laghm --all --icons --git --color=always --group-directories-first"
+alias ll="eza -l --color=always --group-directories-first --icons"
+alias lt="eza -aT --color=always --group-directories-first --icons -I '.git|.vscode|node_modules'"
+alias l="eza -lah --color=always --group-directories-first --icons"
+alias last="find . -type f -not -path "*/\.*" -exec ls -lrt {} +"
+
+# ------------------ passwordstore ------------------
+
+alias p="pass"
+alias pp="pass git push"
+alias pi="pass insert"
+alias pmv="pass mv"
+alias pcp="pass cp"
+alias pedit="pass edit"
+alias psearch="pass search"
+alias psh="pass show"
+alias psc="pass show -c"
+alias ppp="pass show -c Sites/canva.com"
