50db03c2939e5b38232566d995faa2b38a54604c
Dimitri Pietersz <pietersz@gmail.com>
2025-10-26 02:14:54 +0200

    fix(waybar): update cpugovernor to use powersave instead of ondemand
    - Changed cpugovernor script to cycle between 'performance' and 'powersave' governors
    - 'ondemand' governor is not available on this system
    - Requires sudoers configuration to allow passwordless cpupower execution
    - Module now properly switches CPU governors on click

--- dot_config/waybar/custom_modules/executable_cpugovernor.sh
+++ dot_config/waybar/custom_modules/executable_cpugovernor.sh
@@ -5,11 +5,11 @@ GOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 if [ $GOVERNOR = performance ]; then
   echo '{"text": "perf", "alt": "perf", "class": "performance", "tooltip": "<b>Governor</b> Performance"}'
   if [[ $1 = switch ]]; then
-    sudo cpupower frequency-set -g ondemand
+    sudo cpupower frequency-set -g powersave
     pkill -RTMIN+8 waybar
   fi
-elif [ $GOVERNOR = ondemand ]; then
-  echo '{"text": "ondemand", "alt": "ondemand", "class": "ondemand", "tooltip": "<b>Governor</b> On Demand"}'
+elif [ $GOVERNOR = powersave ]; then
+  echo '{"text": "powersave", "alt": "powersave", "class": "powersave", "tooltip": "<b>Governor</b> Powersave"}'
   if [[ $1 = switch ]]; then
     sudo cpupower frequency-set -g performance
     pkill -RTMIN+8 waybar
